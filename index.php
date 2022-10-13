ceb84c8aac84baea673b09c9d5769af572625813
rbrecknell <115728972+rbrecknell@users.noreply.github.com>
2022-10-13 17:55:53 +0100

    Changed cert path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "mysql-adapt-dev.mysql.database.azure.com", "symbiotics", "Pukka118!", "trial", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
   die("Connection failed: ".mysqli_connect_error());
