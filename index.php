1ef551bd9c726de1b71e2bea968cbd3847ed06c1
rbrecknell <115728972+rbrecknell@users.noreply.github.com>
2022-10-13 17:41:05 +0100

    DB test

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello Matt!";
+// echo "Hello Matt!";
+
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "/site/wwwroot/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "mysql-adapt-dev.mysql.database.azure.com", "symbiotics", "Pukka118!", "trial", 3306, MYSQLI_CLIENT_SSL);
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
