7f41793bf43de1a2a0a4266cd213a13c7ddfb45f
rbrecknell <115728972+rbrecknell@users.noreply.github.com>
2022-10-13 17:58:15 +0100

    syntax

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "mysql-adapt-dev.mysql.database.azure.com", "symbiotics", "Pukka118!", "trial", 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
+if (mysqli_connect_errno()) {
   die("Connection failed: ".mysqli_connect_error());
 }
